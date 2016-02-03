8e03a492a9b5d515f9df341677bee585b2c4b18b
stain88 <marcbaghdadi@gmail.com>
2016-02-03 22:57:38 +0000

    update text box with future date

--- task2/scripts/app.js
+++ task2/scripts/app.js
@@ -1,12 +1,16 @@
 $(document).ready(start);
 
 function start() {
+  $('#laterdate').datepicker({dateFormat: 'dd/mm/yy'}).datepicker("option", "disabled", true);
   $("#datepicker").datepicker({
     onSelect: function() {
       var selectedDate = $('#datepicker').datepicker('getDate');
       selectedDate.setDate(selectedDate.getDate()+5);
-      $('#laterdate').text("setDate", selectedDate);
-      console.log("selected: " + selectedDate);
+      updateDate(selectedDate);
     }
   });
+}
+
+function updateDate(date) {
+  $('#laterdate').datepicker("setDate", date);
 }
