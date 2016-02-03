00faa29d7a16a545aef36aeab7c21861923b2a11
stain88 <marcbaghdadi@gmail.com>
2016-02-03 22:32:37 +0000

    return future date

--- task2/scripts/app.js
+++ task2/scripts/app.js
@@ -3,7 +3,10 @@ $(document).ready(start);
 function start() {
   $("#datepicker").datepicker({
     onSelect: function() {
-      console.log($('#datepicker').datepicker('getDate'));
+      var selectedDate = $('#datepicker').datepicker('getDate');
+      selectedDate.setDate(selectedDate.getDate()+5);
+      $('#laterdate').text("setDate", selectedDate);
+      console.log("selected: " + selectedDate);
     }
   });
 }
